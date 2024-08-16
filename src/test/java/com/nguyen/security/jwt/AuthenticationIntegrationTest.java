6d27e9c122c81109225a06047614df18684c2c8d
dinhnguyentlh <dinhnguyentlh@gmail.com>
2024-08-16 09:21:24 +0700

    first commit

---
+++ src/test/java/com/nguyen/security/jwt/AuthenticationIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.nguyen.security.jwt;
+
+import com.nguyen.config.SecurityConfiguration;
+import com.nguyen.config.SecurityJwtConfiguration;
+import com.nguyen.config.WebConfigurer;
+import com.nguyen.management.SecurityMetersService;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import tech.jhipster.config.JHipsterProperties;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    classes = {
+        JHipsterProperties.class,
+        WebConfigurer.class,
+        SecurityConfiguration.class,
+        SecurityJwtConfiguration.class,
+        SecurityMetersService.class,
+        JwtAuthenticationTestUtils.class,
+    }
+)
+public @interface AuthenticationIntegrationTest {
+}
